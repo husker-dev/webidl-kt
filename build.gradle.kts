0f24316468fae76a4664f064e6483b8f56838528
nshtengauer <nshtengauer@corp.finam.ru>
2026-01-11 20:15:06 +0300

    Added maven publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.maven.publish)
 }
 
 group = "com.huskerdev"
@@ -51,5 +52,38 @@ kotlin {
 
     sourceSets.commonTest.dependencies {
         implementation(kotlin("test"))
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(group.toString(), "webidl-kt", version.toString())
+
+    pom {
+        name = "webidl-kt"
+        description = "Kotlin Multiplatform WebIDL parsing library"
+        url = "https://github.com/husker-dev/webidl-kt"
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "husker-dev"
+                name = "Nikita Shtengauer"
+                email = "redfancoestar@gmail.com"
+            }
+        }
+        scm {
+            connection = "https://github.com/husker-dev/webidl-kt.git"
+            developerConnection = "https://github.com/husker-dev/webidl-kt.git"
+            url = "https://github.com/husker-dev/webidl-kt"
+        }
     }
 }
